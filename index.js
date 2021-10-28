30e0d6c2e4622a08bb80d9f9e8702803a6e1f0c7
Naveed Mustafa <mustafanaveed404@gmail.com>
2021-10-29 01:24:20 +0600

    fourth commit

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 console.log("hello world");
-console.log("Hello from the othe side");
-// git add "file name" likhe stage korte hbe then, commit krte hbe
+
+// git add "file name" likhe commit krte hbe 
